2404664c4e0bfc0774c2cb98ef4863251a698167
James O'Brien <james@blitzm.com>
2022-12-02 08:58:16 +1100

    Part 2 implementation

--- problem-1/solve.sh
+++ problem-1/solve.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 v=0
-m=0
-t=0
-while read p;
-do
-[[ $p = "" ]] && v=0 || v=$(($v + $p))
-(($v > $m)) && m=$v
-done < input-data.txt
-(($v > $m)) && m=$v
-echo $m
+e1=0
+e2=0
+e3=0
+function compute(){
+    (($v > $e1)) && e3=$e2 && e2=$e1 e1=$v || \
+    { (($v > $e2)) && e3=$e2 && e2=$v || \
+    { (($v > $e3)) && e3=$v; } ;}; }
+while read p; do
+    [[ $p = "" ]] && v=0 || v=$(($v + $p))
+    compute
+done < input-data.txt;
+v=$(($v + $p))
+compute
+echo "$(($e1 + $e2 + $e3))"
